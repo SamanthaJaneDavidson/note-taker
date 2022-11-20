ca9bd0acdbc49a3e50b9de3620312ec2641e0390
Samantha Davidson <samanthajanedavidson@gmail.com>
2022-11-20 15:45:12 -0500

    Updated server file with new post data

--- server.js
+++ server.js
@@ -1,68 +1,52 @@
-// const { urlencoded } = require(`express`); // VSC did this on it's own...
 const express = require(`express`);
-// const { appendFile, fstat } = require(`fs`); // VSC did this on it's own...
-// const { restart } = require("nodemon");
 const path = require(`path`);
 const data = require(`./db/db.json`);
-const fs = require(`fs`)
-const uuid = require(`./helpers/uuid`)
+const uuid = require("./helpers/uuid");
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 7001;
 
 // Middleware 
 app.use(express.static(`public`));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Get route for homepage
-app.get(`/`, (req, res) => {
-    res.sendFile(path.join(__dirname, `/public/index.html`))
+// Get for homepage 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-
-// Get route for notes page 
-app.get(`/api/notes`, (req, res) => {
-    res.sendFile(path.join(__dirname, `/public/notes.html`))
+// Get for notes data 
+app.get('/api/notes', (req, res) => {
+    res.json(data);
 });
 
-// app.get(`*`, (req, res) => {
-//     res.sendFile(path.join(__dirname, `public/index.html`))
-// });
-
-// Get API routes
-
-// Post for new note
-app.post(`api/notes`, (req, res) => {
-
+// Post for notes data 
+app.post(`/api/notes`, (req, res) => {
+    
     const { title, text } = req.body;
 
-    if(req.body) {
+    if (title && text) {
         const newNote = {
             title,
             text,
-            note_id: uuid(),
+            id: uuid(),
         };
-        
+
+        const response = {
+            status: `Note added`,
+            body: newNote,
+        };
+
+        console.log(response);
+        res.status(200).json(response);
+    } else {
+        res.status(500).json(`Error posting note`);
     }
-
-});
-
-
-    // if (typeof req.body.note !== `undefined`) {
-    //     data.push({ ...req.body })
-    //     console.log(note)
-    //     return fs.writeFile(path.join(__dirname, "./db/db.json", JSON.stringify(data), (err) => {
-    //         err ? console.error(err) : console.log("Note written")
-    //         return res.status(200).json(req.body)
-    //     }));
-    // }
-    //     return res.status(500).send("Error");
+    });
 
 
-
-// Listen
+// Listen 
 app.listen(PORT, () => {
-    console.log(`App listening at http://localhost:${PORT}`)
+    console.log(`App is listening at http://localhost:${PORT}/`);
 });
-
