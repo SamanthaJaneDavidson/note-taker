cf69725ec60c0a2dff0fe81acf106eafc4ac9065
Samantha Davidson <samanthajanedavidson@gmail.com>
2022-11-21 13:00:01 -0500

    Updated server file to include get for notes; updated file write to db.json

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -31,7 +31,7 @@ const getNotes = () =>
     headers: {
       'Content-Type': 'application/json',
     },
-  });
+  })
 
 const saveNote = (note) =>
   fetch('/api/notes', {
@@ -40,7 +40,7 @@ const saveNote = (note) =>
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(note),
-  });
+  })
 
 const deleteNote = (id) =>
   fetch(`/api/notes/${id}`, {
@@ -118,6 +118,7 @@ const handleRenderSaveBtn = () => {
 
 // Render the list of note titles
 const renderNoteList = async (notes) => {
+  console.log(notes)
   let jsonNotes = await notes.json();
   if (window.location.pathname === '/notes') {
     noteList.forEach((el) => (el.innerHTML = ''));
--- server.js
+++ server.js
@@ -1,19 +1,24 @@
-const express = require(`express`);
-const path = require(`path`);
-const data = require(`./db/db.json`);
-const uuid = require("./helpers/uuid");
+const express = require('express');
+const path = require('path');
+const data = require('./db/db.json');
+const uuid = require('./helpers/uuid');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 7001;
 
 // Middleware 
-app.use(express.static(`public`));
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Get for homepage 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.send(path.join(__dirname, "public", "index.html"));
+});
+
+app.get('/notes', (req, res) => {
+    res.send(path.join(__dirname, "public", "notes.html"));
 });
 
 // Get for notes data 
@@ -22,8 +27,8 @@ app.get('/api/notes', (req, res) => {
 });
 
 // Post for notes data 
-app.post(`/api/notes`, (req, res) => {
-    
+app.post('/api/notes', (req, res) => {
+
     const { title, text } = req.body;
 
     if (title && text) {
@@ -34,17 +39,25 @@ app.post(`/api/notes`, (req, res) => {
         };
 
         const response = {
-            status: `Note added`,
+            status: 'Note added',
             body: newNote,
         };
 
-        console.log(response);
-        res.status(200).json(response);
-    } else {
-        res.status(500).json(`Error posting note`);
-    }
-    });
+        // // if {
+        //     data.push({ ...req.body })
+            console.log(newNote)
+            return fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(data), (err) => {
+                err ? console.error(err) : console.log("Note written")
+                return res.status(200).json(response)
+            });
 
+        // } else {
+        //     res.status(500).json(`Error posting note`);
+        // }
+    }
+});
+
+// Add delete app
 
 // Listen 
 app.listen(PORT, () => {
